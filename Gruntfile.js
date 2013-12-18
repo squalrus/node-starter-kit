6d353803e87b99f09ef24e6404c46cb0add29524
Chad Schulz <chad.awesome@gmail.com>
2013-12-17 21:57:51 -0800

    Comma and spacing updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,62 @@
 (function() {
-    'use strict';
+    "use strict";
 
-    module.exports = function( grunt ){
+    module.exports = function (grunt) {
 
         // Project Configuration
         grunt.initConfig({
-             pkg: grunt.file.readJSON( 'package.json' )
+             pkg: grunt.file.readJSON("package.json"),
 
-             // Uglify JS
-            ,uglify: {
-                 options: { }
-                ,dist: {
+            // Uglify JS
+            uglify: {
+                options: {},
+                dist: {
                     files: {
-                         // 'public/dest/file.js' : ['source.js']
+                         // "public/dest/file.js" : ["source.js"]
                     }
                 }
-            }
+            },
 
             // Compile LESS
-            ,less: {
+            less: {
                 development: {
                     options: {
                         compress: true
-                    }
-                    ,files: {
-                         // 'public/css/dest.min.css': 'source.less'
+                    },
+                    files: {
+                         // "public/css/dest.min.css": "source.less"
                     }
                 }
-            }
+            },
 
             // Linting Files / Directories
-            ,jshint: {
-                all: ['Gruntfile.js', 'lib/*.js']
-                ,options: {
+            jshint: {
+                all: ["Gruntfile.js", "lib/*.js"],
+                options: {
                     laxcomma: true,
                     eqeqeq: true,
-                    quotmark: 'single',
+                    quotmark: "single",
                     unused: true,
                     strict: true,
                     trailing: true
                 }
-            }
+            },
 
             // Watch Directories / Files
-            ,watch: {
-                 files: ['Gruntfile.js', 'lib/*.js', 'theme/*.less']
-                ,tasks: ['default']
+            watch: {
+                 files: ["Gruntfile.js", "lib/*.js", "theme/*.less"]
+                ,tasks: ["default"]
             }
         });
 
         // Load the plugins
-        grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-        grunt.loadNpmTasks( 'grunt-contrib-less' );
-        grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-        grunt.loadNpmTasks( 'grunt-contrib-watch' );
+        grunt.loadNpmTasks("grunt-contrib-uglify");
+        grunt.loadNpmTasks("grunt-contrib-less");
+        grunt.loadNpmTasks("grunt-contrib-jshint");
+        grunt.loadNpmTasks("grunt-contrib-watch");
 
         // Default tasks
-        grunt.registerTask( 'default', ['uglify', 'less', 'jshint'] );
+        grunt.registerTask("default", ["uglify", "less", "jshint"]);
     };
 
 })();
--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-var express      = require( 'express' )
-    ,http        = require( 'http' )
-    ,path        = require( 'path' )
-    ,socketio     = require( 'socket.io' )
-    ,app         = express()
-    ,io
-    ,server;
+var express     = require('express'),
+    http        = require('http'),
+    path        = require('path'),
+    socketio    = require('socket.io'),
+    app         = express(),
+    io,
+    server;
 
 // Environment Settings
-app.set( 'port', process.env.PORT || 3000 );
-app.set( 'views', __dirname + '/views' );
-app.set( 'view engine', 'ejs' );
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 // Middleware
-app.use( express.favicon() );
-app.use( express.bodyParser() );
-app.use( express.compress() );
-app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.compress());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routing
-app.get( '/', function( req, res ){
-    res.render( 'index' );
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
 
 // Create Server
-server = http.createServer( app )
+server = http.createServer(app);
 
 // Bind Socket.io
-io = socketio.listen( server );
+io = socketio.listen(server);
 
 // Start Server
-server.listen( app.get( 'port' ), function( ){
-    console.log( 'Server listening on port ' + app.get( 'port' ) );
+server.listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
 });
 
 // Handle Socket Connections
-io.on('connection', function( socket ){
-    setTimeout(function(){
+io.on('connection', function (socket) {
+    setTimeout(function () {
     	socket.emit('lub');
-    },1000)
+    }, 1000)
 });
 
